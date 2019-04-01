58e87cab9d091149fabc0e9c265301a33bac5a70
Loy Vanich <laploy@gmail.com>
2019-04-02 05:02:34 +0700

    Add files via upload

---
+++ Project/ShortHandIf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            int c = 0;
+
+            if(a > b)
+            {
+                c = 1;
+            }
+            else
+            {
+                c = 2;
+            }
+
+            // ------ shorthand if
+            c = a > b ? 1 : 2;
+        }
+    }
+}
